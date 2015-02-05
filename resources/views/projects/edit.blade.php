34e7b7cb6637aef00c98cb087053b02793b51348
flynsarmy <flynsarmy@gmail.com>
2015-02-05 21:09:40 +1000

    Button text fix

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -4,6 +4,6 @@
     <h2>Edit Project</h2>
 
     {!! Form::model($project, ['method' => 'PATCH', 'route' => ['projects.update', $project->slug]]) !!}
-        @include('projects/partials/_form', ['submit_text' => 'Create Project'])
+        @include('projects/partials/_form', ['submit_text' => 'Edit Project'])
     {!! Form::close() !!}
 @endsection
